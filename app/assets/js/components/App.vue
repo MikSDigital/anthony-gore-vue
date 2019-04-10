15dfcb704f11220848faccc8003fe8f6b090f709
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-04-10 09:01:34 +0300

    generating calendar completed

--- app/assets/js/components/App.vue
+++ app/assets/js/components/App.vue
@@ -13,16 +13,40 @@
        },
        computed: {
            days() {
+               const SUNDAY = 0;
+               const MONDAY = 1;
+
+               // Generating all days in the current month
                let days = [];
                let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
 
                do {
                    days.push(currentDay);
-                   currentDay = this.$moment(currentDay).add(1, 'day');
+                   currentDay = this.$moment(currentDay).add(1, 'days');
 
                    console.log(currentDay);
 
                } while ((currentDay.month() + 1) === this.month);
+
+               // Add previous (padding) days to start
+               currentDay = this.$moment(days[0]);
+
+               if (currentDay.day() !== MONDAY) {
+                   do {
+                       currentDay = this.$moment(currentDay).subtract(1,'days');
+                       days.unshift(currentDay);
+                   } while (currentDay.day() !== MONDAY);
+               }
+               
+                // Add padding days to the month end
+               currentDay = this.$moment(days[days.length - 1]);
+
+               if (currentDay.day() !== SUNDAY) {
+                   do {
+                       currentDay = this.$moment(currentDay).add(1,'days');
+                       days.push(currentDay);
+                   } while (currentDay.day() !== SUNDAY);
+               }
 
                return days;
            }
